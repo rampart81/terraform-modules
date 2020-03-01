168b83cbbc7239b5c92d555457809bfef474a1eb
송은우 <song-eun-u@song-eun-uui-MacBookPro.local>
2020-03-01 13:38:53 +0900

    wip

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,51 +2,37 @@
 # VPC 
 ############################################################
 resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.cidr_block}"
-  enable_dns_support   = "${var.enable_dns_support}" 
-  enable_dns_hostnames = "${var.enable_dns_hostnames}" 
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_dns_support 
+  enable_dns_hostnames = var.enable_dns_hostnames 
 
-  tags { Name = "${var.name}" }
+  tags { Name = var.name }
 }
 
 ############################################################
 # Public Subnets
 ############################################################
-resource "aws_subnet" "public_availability_zone1" {
-	vpc_id            = "${aws_vpc.vpc.id}"
-	cidr_block        = "${var.public_availability_zone1_cidr_block}"
-	availability_zone = "${var.availability_zone1}"
+resource "aws_subnet" "public_availability_zones" {
+  count             = length(var.public_availabile_zones)
+	vpc_id            = aws_vpc.vpc.id
+	cidr_block        = var.public_availabile_zones[count.indedx].cidr_block
+	availability_zone = var.public_availabile_zones[count.indedx].zone
 
-  tags { Name = "${var.name} Public Availability Zone 1" }
-}
-
-resource "aws_subnet" "public_availability_zone2" {
-	vpc_id            = "${aws_vpc.vpc.id}"
-	cidr_block        = "${var.public_availability_zone2_cidr_block}"
-	availability_zone = "${var.availability_zone2}"
-  
-  tags { Name = "${var.name} Public Availability Zone 2" }
-
+  tags { Name = "${var.name} Public Availability Zone ${count.index}" }
 }
 
 ############################################################ 
 # Private Subnets
 ############################################################
-resource "aws_subnet" "private_availability_zone1" {
-  vpc_id            = "${aws_vpc.vpc.id}"
-	cidr_block        = "${var.private_availability_zone1_cidr_block}"
-  availability_zone = "${var.availability_zone1}"
+resource "aws_subnet" "private_availability_zones" {
+  count             = length(var.private_availabile_zones)
+	vpc_id            = aws_vpc.vpc.id
+	cidr_block        = var.private_availabile_zones[count.indedx].cidr_block
+	availability_zone = var.private_availabile_zones[count.indedx].zone
 
-  tags { Name = "${var.name} Private Availability Zone 1" }
+  tags { Name = "${var.name} Private Availability Zone ${count.index}" }
 }
 
-resource "aws_subnet" "private_availability_zone2" {
-  vpc_id            = "${aws_vpc.vpc.id}"
-	cidr_block        = "${var.private_availability_zone2_cidr_block}"
-  availability_zone = "${var.availability_zone2}"
-
-  tags { Name = "${var.name} Private Availability Zone 1" }
-}
 
 ############################################################
 # EIP
@@ -59,60 +45,62 @@ resource "aws_eip" "nat" {
 # Routing table for public subnets
 ############################################################
 resource "aws_internet_gateway" "ig" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   tags { Name = "${var.name} Internet Gateway" }
 }
 
 resource "aws_default_route_table" "default" {
-  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
 
 
   tags { Name = "${var.name} Public Default Route Table" }
 }
 
 resource "aws_route" "public" {
-	route_table_id         = "${aws_vpc.vpc.default_route_table_id}"
+	route_table_id         = aws_vpc.vpc.default_route_table_id
 	destination_cidr_block = "0.0.0.0/0"
-	gateway_id             = "${aws_internet_gateway.ig.id}"
+	gateway_id             = aws_internet_gateway.ig.id
 }
 
-resource "aws_route_table_association" "public1" {
-	subnet_id      = "${aws_subnet.public_availability_zone1.id}"
-	route_table_id = "${aws_vpc.vpc.default_route_table_id}"
-}
-
-resource "aws_route_table_association" "public2" {
-	subnet_id      = "${aws_subnet.public_availability_zone2.id}"
-	route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_availabile_zones)
+	subnet_id      = aws_subnet.public_availability_zones[count.index].id
+	route_table_id = aws_vpc.vpc.default_route_table_id
 }
 
 ############################################################
 # Routing table for private subnets
 ############################################################
 resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.public_availability_zone1.id}"
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_availability_zones[0].id
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   tags { Name = "${var.name} Private Route Table" }
 }
 
 resource "aws_route" "private" {
-	route_table_id         = "${aws_route_table.private.id}"
+	route_table_id         = aws_route_table.private.id
 	destination_cidr_block = "0.0.0.0/0"
-	nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+	nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private1" {
-	subnet_id      = "${aws_subnet.private_availability_zone1.id}"
-	route_table_id = "${aws_route_table.private.id}"
+	subnet_id      = aws_subnet.private_availability_zone1.id
+	route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private2" {
-	subnet_id      = "${aws_subnet.private_availability_zone2.id}"
-	route_table_id = "${aws_route_table.private.id}"
+	subnet_id      = aws_subnet.private_availability_zone2.id
+	route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_availabile_zones)
+	subnet_id      = aws_subnet.private_availability_zones[count.index].id
+	route_table_id = aws_vpc.vpc.default_route_table_id
 }
